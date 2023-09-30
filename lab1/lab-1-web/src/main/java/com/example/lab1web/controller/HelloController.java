1a803d55bead38f0aff6cf9def3104dcdcfeaa64
romalitv <youlikeyoupass@gmail.com>
2023-09-30 13:55:35 +0300

    added Console and Web applications

---
+++ lab1/lab-1-web/src/main/java/com/example/lab1web/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.lab1web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    @ResponseBody
+    String helloWorld() {
+        return "Hello World!1";
+    }
+}
