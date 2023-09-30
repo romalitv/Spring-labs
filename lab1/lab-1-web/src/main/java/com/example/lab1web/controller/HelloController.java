699189c401f7fd68dad05cc9d945bf6a39944daf
romalitv <youlikeyoupass@gmail.com>
2023-09-30 14:20:09 +0300

    added static HTML-pages about us

--- lab1/lab-1-web/src/main/java/com/example/lab1web/controller/HelloController.java
+++ lab1/lab-1-web/src/main/java/com/example/lab1web/controller/HelloController.java
@@ -10,6 +10,31 @@ public class HelloController {
     @GetMapping("/")
     @ResponseBody
     String helloWorld() {
-        return "Hello World!1";
+        return "Type our names in URL! " + "<br>" +
+                "/roma , /ivan , /mykhailo";
+    }
+
+    @GetMapping("/roma")
+    @ResponseBody
+    String romaInfo() {
+        return "Roma Lytvyniuk" + "<br>" +
+                "Birthday: 06.03.2004" + "<br>" +
+                "Group: IM-13";
+    }
+
+    @GetMapping("/ivan")
+    @ResponseBody
+    String ivanInfo() {
+        return "Ivan Lotokhin" + "<br>" +
+                "Birthday: 15.01.2004" + "<br>" +
+                "Group: IM-13";
+    }
+
+    @GetMapping("/mykhailo")
+    @ResponseBody
+    String mykhailoInfo() {
+        return "Moliakov Mykhailo" + "<br>" +
+                "Birthday: 28.12.2003" + "<br>" +
+                "Group: IM-13";
     }
 }
